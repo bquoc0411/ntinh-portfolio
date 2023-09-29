c052690c2bd19dfc20486149f1b632f794f0524e
bquoc0411 <hoangquocbinh0411@gmail.com>
2023-09-29 17:25:56 +0700

    fix: set-minHeight for detail pages

--- src/components/ProjectDetail.jsx
+++ src/components/ProjectDetail.jsx
@@ -6,25 +6,27 @@ import Footer from "./Footer";
 const ProjectDetail = (props) => {
   return (
     <>
-      <Navbar />
-      <section className="container">
-        <h1 className={style.weddingTitle}>{props.heading}</h1>
-        <div className="row">
-          {props.data?.map((item) => {
-            return (
-              <div className="col-lg-6 mt-5" key={item.id}>
-                <iframe
-                  style={{ width: "100%", height: "300px" }}
-                  src={item.link}
-                  title={item.title}
-                  allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-                  allowFullScreen
-                ></iframe>
-              </div>
-            );
-          })}
-        </div>
-      </section>
+      <div className={style.viewH}>
+        <Navbar />
+        <section className="container">
+          <h1 className={style.weddingTitle}>{props.heading}</h1>
+          <div className="row">
+            {props.data?.map((item) => {
+              return (
+                <div className="col-lg-6 mt-5" key={item.id}>
+                  <iframe
+                    style={{ width: "100%", height: "300px" }}
+                    src={item.link}
+                    title={item.title}
+                    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+                    allowFullScreen
+                  ></iframe>
+                </div>
+              );
+            })}
+          </div>
+        </section>
+      </div>
       <Footer />
     </>
   );
